bbd363e48090854e74fa043b8e55ef023290f722
smarcd <sunnym@gmail.com>
2026-05-06 18:25:56 +0300

    feat(vfs): add base image builder (#5)

--- packages/kernel/src/index.ts
+++ packages/kernel/src/index.ts
@@ -1,6 +1,14 @@
-export { Sandbox } from './sandbox.js';
-export type { RunResult } from './run-result.js';
-export { NodeAdapter } from './platform/node-adapter.js';
-export { OverlayVFS } from './vfs/overlay-vfs.js';
-export { NodeDirectoryRootProvider } from './vfs/node-directory-root-provider.js';
-export type { RootProvider, RootProviderStat } from './vfs/root-provider.js';
+export { Sandbox } from "./sandbox.js";
+export type { RunResult } from "./run-result.js";
+export { NodeAdapter } from "./platform/node-adapter.js";
+export { OverlayVFS } from "./vfs/overlay-vfs.js";
+export { NodeDirectoryRootProvider } from "./vfs/node-directory-root-provider.js";
+export type { RootProvider, RootProviderStat } from "./vfs/root-provider.js";
+export { buildBaseImage } from "./base-image/build-base-image.js";
+export type {
+  BaseImageFile,
+  BaseImageManifest,
+  BaseImageSymlink,
+  BaseImageTool,
+  BuildBaseImageOptions,
+} from "./base-image/build-base-image.js";
