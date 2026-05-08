17d1d82b94f792d574ba7de165b399d9afd8a0b2
sunnymar <sunnym@gmail.com>
2026-05-08 18:29:11 +0300

    fix: remove busybox include shims

--- test-fixtures/c-ports/busybox/compat/include/sys/sysmacros.h
+++
@@ -1,9 +0,0 @@
-#ifndef YURT_BUSYBOX_COMPAT_SYS_SYSMACROS_H
-#define YURT_BUSYBOX_COMPAT_SYS_SYSMACROS_H
-
-#define major(dev) ((unsigned int)(((dev) >> 8) & 0xfff))
-#define minor(dev) ((unsigned int)(((dev) & 0xff) | (((dev) >> 12) & 0xfff00)))
-#define makedev(ma, mi) \
-	((((ma) & 0xfff) << 8) | ((mi) & 0xff) | (((mi) & 0xfff00) << 12))
-
-#endif
